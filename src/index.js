a50a5c5262977a948bd2f275fa894e42ff1522b7
fengzi2016 <1820166258@qq.com>
2018-01-03 21:21:49 +0800

    lesson-demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class Index extends Component{
+    render(){
+      return (
+        <div>
+          <LessonsList lessons = {lessons} />
+        </div>
+        
+        )
+    }
+  }
+  class Lesson extends Component {
+    print(){
+      const index = this.props.in;
+      console.log(index+ '-' + this.props.lesson.title);
+    }
+    render(){
+      const lesson = this.props.lesson
+      return (
+        <div onClick = {this.print.bind(this)} >
+          <h1>{lesson.title}</h1>
+          <p>{lesson.description}</p>
+        </div>
+        )
+    }
+  }
+  
+  class LessonsList extends Component {
+    render(){
+      const lessons =this.props.lessons
+      return(
+        <div>
+        {lessons.map((les,index)=>
+          <Lesson lesson = {les} key = {index} in = {index} />
+        )}
+        </div>
+        )
+    }
+  }
+  
+  
+  const lessons = [
+    { title: 'Lesson 1: title', description: 'Lesson 1: description' },
+    { title: 'Lesson 2: title', description: 'Lesson 2: description' },
+    { title: 'Lesson 3: title', description: 'Lesson 3: description' },
+    { title: 'Lesson 4: title', description: 'Lesson 4: description' }
+
+  ]
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+)
