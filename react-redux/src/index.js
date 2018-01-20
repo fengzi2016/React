6744fa4676d0621470b4a27d3f04fcba75f24cf7
fengzi2016 <1820166258@qq.com>
2018-01-20 22:10:54 +0800

    update

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
+import {Provider} from './react-redux'
 function createStore(reducer){
     let state = null
     const listeners = []
@@ -31,15 +32,6 @@ const themeReducer = (state, action) => {
 const store = createStore(themeReducer)
 
 class Index extends Component {
-    static childContextTypes = {
-        store: PropTypes.object
-      }
-    
-      getChildContext () {
-        return { store }
-        
-      }
-    
   render () {
     return (
       <div>
@@ -51,6 +43,8 @@ class Index extends Component {
 }
 
 ReactDOM.render(
-  <Index />,
+  <Provider store={store}>
+    <Index /> 
+  </Provider>,
   document.getElementById('root')
 )
